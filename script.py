40c320b838ee054c736ede7538e443b564bec5a3
aziz <aziz.ensa@uhp.ac.ma>
2022-10-04 02:27:12 -0700

    adding fixtur mechanisme

--- script.py
+++ script.py
@@ -1,6 +1,3 @@
-def add(x, y):
+def add(x):
 
-    return x + y
-
-
-print(f"this is the sum: 1 , 2 , {add(1,2)}")
+    return x + 1
--- script_test.py
+++ script_test.py
@@ -1,6 +1,20 @@
 from script import add
 
 
-def test_add():
+'''def test_add():
 
-    assert 1 + 2 == add(1, 2)
+    assert 1 + 2 == add(1, 2)'''
+
+def setup_function(function):
+
+   print(f'Runing Setup: {function.__name__}')
+   function.x = 10
+
+def teardown_function(function):
+
+   print(f'Runing Teardown: {function.__name__}')
+   del function.x
+
+def test_script_add():
+
+   assert add(test_script_add.x) == 11
